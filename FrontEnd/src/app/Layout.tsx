94f5693e475d1491a172f83bee80c78d4984c16b
Lomins <deadlomins@gmail.com>
2026-03-30 10:42:55 -0500

    feat: add Banner component with image carousel and styles

--- FrontEnd/src/app/Layout.tsx
+++ FrontEnd/src/app/Layout.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header/Header';
+import Banner from '../components/Banner/Banner';
 import './Layout.css';
+
 
 export const Layout = () => {
   return (
     <>
       <Header />
+      <Banner />
     </>
   );
 };
