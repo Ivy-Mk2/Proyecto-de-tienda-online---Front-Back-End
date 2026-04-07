a240f5026e4e05fe052d743d68f1c970c35f66cc
Ivy-Mk2 <119135234+Ivy-Mk2@users.noreply.github.com>
2026-04-07 03:25:03 -0500

    feat(frontend): add dynamic featured section integrated in layout

--- FrontEnd/src/app/Layout.tsx
+++ FrontEnd/src/app/Layout.tsx
@@ -1,14 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header/Header';
 import Banner from '../components/Banner/Banner';
+import Featured from '../components/Featured/Featured';
 import './Layout.css';
-
 
 export const Layout = () => {
   return (
     <>
       <Header />
       <Banner />
+      <Featured />
+      <Outlet />
     </>
   );
 };
