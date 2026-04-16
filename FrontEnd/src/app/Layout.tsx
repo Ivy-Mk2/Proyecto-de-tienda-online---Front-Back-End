a03e6e8d5cdb6001fd437f2b2947572310cfa463
Lomins <deadlomins@gmail.com>
2026-04-16 18:14:38 -0500

    feat: restructure layout by moving components to HomePage and removing unused hooks

--- FrontEnd/src/app/Layout.tsx
+++ FrontEnd/src/app/Layout.tsx
@@ -1,28 +1,9 @@
-import Header from '../components/Header/Header';
-import Banner from '../components/Banner/Banner';
-import SpecialBanner from '../components/SpecialBanner/SpecialBanner';
-import Featured from '../components/Featured/Featured';
-import Marquee from '../components/Marquee/Marquee';
-import SaleBanner from  '../components/SaleBanner/SaleBanner';
-import BodySection from '../components/BodySection/BodySection';
-import Announcement from '../components/Announcement/Announcement';
-import Newsletter from '../components/Newsletter/Newsletter';
-import Footer from '../components/Footer/Footer';
 import './Layout.css';
-
+import { HomePage } from '../pages/HomePage';
 export const Layout = () => {
   return (
     <>
-      <Header />
-      <Banner />
-      <Featured />
-      <SpecialBanner />
-      <Marquee />
-      <SaleBanner />  
-      <BodySection />
-      <Announcement />
-      <Newsletter />
-      <Footer />
+      <HomePage />
     </>
   );
 };
